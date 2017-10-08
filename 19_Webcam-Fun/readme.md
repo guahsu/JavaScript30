88930d557d547578c7a982168aa2abff0eb99158
GuaHsu <guaswork@gmail.com>
2017-10-08 22:02:01 +0800

    update

---
+++ 19_Webcam-Fun/readme.md
@@ -0,0 +1,1 @@
+# 19 - Webcam Fun
--- 19_Webcam-Fun/scripts.js
+++ 19_Webcam-Fun/scripts.js
@@ -6,47 +6,99 @@ const snap = document.querySelector('.snap');
 
 
 function getVideo() {
-    navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: false
-        })
-        .then(localMediaStream => {
-            /* console.log(localMediaStream); */
-            video.src = window.URL.createObjectURL(localMediaStream);
-            video.play();
-        })
-        .catch(err => {
-            console.error(`ERROR: `, err);
-        })
+  navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then(localMediaStream => {
+      /* console.log(localMediaStream); */
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`ERROR: `, err);
+    })
 }
 
 function paintToCanavas() {
-    const width = video.videoWidth;
-    const height = video.videoHeight;
-    canvas.width = width;
-    canvas.height = height;
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
-    return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height);
-        //
-        const pixels = ctx.getImageData(0, 0, width, height);
-        console.log(pixels);
-        debugger;
-    }, 16)
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // 取得pixels
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // 製作效果
+    /* pixels = redEffect(pixels); */
+    /* pixels = rgbSplit(pixels);
+       ctx.globalAlpha = 0.62; */
+     pixels = greenScreen(pixels);
+    // 置入效果
+    ctx.putImageData(pixels, 0, 0);
+
+    //debugger;
+  }, 16)
 }
 
 function takePhoto() {
-    // 拍照的音效
-    snap.currentTime = 0;
-    snap.play();
-    // 拍照的功能
-    const data = canvas.toDataURL('image/jpeg');
-    const link = document.createElement('a');
-    link.href = data;
-    link.setAttribute('download', 'photo');
-    /* link.textContent = 'Download Image'; */
-    link.innerHTML = `<img src="${data}" alt="photo" />`;
-    strip.insertBefore(link, strip.firstChild);
+  // 拍照的音效
+  snap.currentTime = 0;
+  snap.play();
+  // 拍照的功能
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'photo');
+  /* link.textContent = 'Download Image'; */
+  link.innerHTML = `<img src="${data}" alt="photo" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    pixels.data[i + 500] = pixels.data[i + 1];
+    pixels.data[i - 550] = pixels.data[i + 2];
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
