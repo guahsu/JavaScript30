6d95e502d26a0133516dc870ecece143b45a85a4
GuaHsu <guaswork@gmail.com>
2017-11-01 23:47:52 +0800

    update readme.md

--- 29 - Countdown Timer/scripts-GuaHsu.js
+++ 29 - Countdown Timer/scripts-GuaHsu.js
@@ -13,7 +13,7 @@ function timer(seconds) {
   // 取得時間
   const now = Date.now();
   const timeStamp = now + seconds * 1000;
-  // 開始與結束的時間顯示function
+  // 倒數計時與結束的時間顯示function
   displayTimeLeft(seconds);
   displayEndTime(timeStamp);
   // 計時器執行在countdown裡面方便接著清除使用
@@ -31,33 +31,53 @@ function timer(seconds) {
 
 }
 
+// 顯示倒數時間
 function displayTimeLeft(seconds) {
+  // 透過Math.floor來取得分鐘數(傳入秒數/60取得最大整數)
   const minutes = Math.floor(seconds /60);
+  // 用％來取得傳入秒數除60的餘數（扣除分鐘數後的秒數）
   const remainderSeconds = seconds % 60;
   console.log({minutes, remainderSeconds});
+  // 顯示秒數的部分若小於0數字前補0
   const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  // 顯示對應時間
   document.title = display;
   timerDisplay.textContent = display;
 }
 
+// 顯示結束時間
 function displayEndTime(timestamp) {
+  // 用傳入的timestamp在取得date資訊
   const end = new Date(timestamp);
+  // 從date取得小時數
   const hour = end.getHours();
+  // 轉12小時制
   const adjustedHour = hour > 12 ? hour - 12 : hour;
+  // 從date取得分鐘數
   const minutes = end.getMinutes();
+  // 顯示結束時間，與上方一樣，若分鐘數小於10，則前面補0
   endTime.textContent = `Be Back At ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
+// 開始計時（HTML畫面設定好的時間）
 function startTimer() {
+  // 取得html中設定的data-time（秒數）
   const seconds = parseInt(this.dataset.time);
+  // 傳入計時器function
   timer(seconds);
 }
 
+// 替每個時間按鈕加上監聽click事件，用來啟動計時function
 buttons.forEach(button => button.addEventListener('click', startTimer));
 
+// HTML中的input自訂倒數時間輸入欄位監聽
 document.customForm.addEventListener('submit', function(e) {
+  // 因為用form，submit後避免跳頁使用preventDefault()來阻止預設事件
   e.preventDefault();
+  // 取得input欄位的值
   const mins = this.minutes.value;
+  // 傳入計時器
   timer(mins * 60);
+  // 清空input
   this.reset();
 })
